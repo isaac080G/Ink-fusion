2b55ad0eebf0859e72cb4b85d5409672a63d9ba5
isaac080G <Isaacalberto0836@gmail.com>
2024-03-06 11:35:26 -0600

    all the appearance has been changed, and other static pages have been added.

---
+++ data/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        id:1,
+        nameProduct: 'taza',
+        price:90,
+        image:'images/taza',
+        quantity: 1,
+
+    },
+    {
+        id:2,
+        nameProduct: 'taza glitter silver',
+        price:90,
+        image:'images/taza glitter silver',
+        quantity: 1,
+
+    },
+    {
+        id:3,
+        nameProduct: 'taza glitter rosa',
+        price:90,
+        image:'images/taza glitter rosa',
+        quantity: 1,
+
+    },
+    {
+        id:4,
+        nameProduct: 'taza fondo negro',
+        price:90,
+        image:'images/taza fondo negro',
+        quantity: 1,
+
+    },
+    {
+        id:5,
+        nameProduct: 'cilindro',
+        price:90,
+        image:'images/cilindro',
+        quantity: 1,
+
+    }
+]
